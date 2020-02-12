2c1a800c9cf5034fe0b9925b80c7a3b353ea60b1
andreynho2006 <andreynho2006@gmail.com>
2020-02-12 17:11:37 +0200

    add PropTypes to User, a Back button to user search and a hireable status

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layouts/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class User extends Component {
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
@@ -22,7 +31,20 @@ class User extends Component {
     } = this.props.user;
 
     const { loading } = this.props;
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to search
+        </Link>
+        Hreable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+      </Fragment>
+    );
   }
 }
 
