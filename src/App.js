723c2cf64c34ebeb4a4a23c27d02a4474b10a9d4
andreynho2006 <andreynho2006@gmail.com>
2020-02-12 16:41:56 +0200

    make a method to get a username from github API

--- src/App.js
+++ src/App.js
@@ -11,19 +11,10 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    user: {},
+    loading: false,
+    alert: null
   };
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-
-  //   this.setState({
-  //     users: res.data,
-  //     loading: false
-  //   });
-  // }
 
   // Search github users
   searchUsers = async text => {
@@ -34,6 +25,19 @@ class App extends Component {
 
     this.setState({
       users: res.data.items,
+      loading: false
+    });
+  };
+
+  // get a single github user
+  getUser = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      user: res.data,
       loading: false
     });
   };
