1b6cfac818e303c5c717e3a81f3a3b51c687f49a
andreynho2006 <andreynho2006@gmail.com>
2020-02-12 13:44:15 +0200

    add alert component to app and pass alert from state as a prop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layouts/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -51,11 +52,12 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
