8f7f91785982fc2b34f0569187957938f5b74cb0
andreynho2006 <andreynho2006@gmail.com>
2020-02-12 13:22:02 +0200

    add a clearUsers function that clears users from state

--- src/App.js
+++ src/App.js
@@ -35,12 +35,19 @@ class App extends Component {
     });
   };
 
+  // clear users from state
+  clearUsers = () =>
+    this.setState({
+      users: [],
+      loading: false
+    });
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
