025f32a3f9b25a50edf075587bd84144421d3fed
andreynho2006 <andreynho2006@gmail.com>
2020-02-12 13:38:51 +0200

    make a setAlert function that will set the state of o new object alert

--- src/App.js
+++ src/App.js
@@ -39,8 +39,16 @@ class App extends Component {
   clearUsers = () =>
     this.setState({
       users: [],
-      loading: false
+      loading: false,
+      alert: null
     });
+
+  // set an alert if input is empty
+  setAlert = (message, type) => {
+    this.setState({
+      alert: { msg: message, type: type }
+    });
+  };
 
   render() {
     const { users, loading } = this.state;
@@ -52,6 +60,7 @@ class App extends Component {
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
