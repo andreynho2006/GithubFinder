8c8e44cc56106ce8e909c86f0198b4b225315236
andreynho2006 <andreynho2006@gmail.com>
2020-02-12 17:15:27 +0200

    add a image, name and location details

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -43,6 +43,18 @@ class User extends Component {
         ) : (
           <i className='fas fa-times-circle text-danger' />
         )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt=''
+              className='round-img'
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+        </div>
       </Fragment>
     );
   }
